815220329fcd028263a80725cbc573d62f82ee4b
SydannyOpal <sydanny15@gmail.com>
2022-04-14 00:15:24 -0400

    4.13.22

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
+const transpose = (matrix) => {
+    const newMatrix = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+        const newArray = [];
+        for (let j = 0; j < matrix.length; j++) {
+            newArray.push(matrix[j][i]);
+        }
+        newMatrix.push(newArray);
+    }
+    return newMatrix;
+}
+
+const wordSearch = (letters, word) => {
+    if (word === "") {
+        return false;
+    }
+    if (letters.length === 0 || letters[0].length === 0) {
+        return false;
+    }
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.toUpperCase().includes(word.toUpperCase())) {
+            return true;
+        }
     }
+    const newMatrix = transpose(letters);
+    const verticalJoin = newMatrix.map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.toUpperCase().includes(word.toUpperCase())) {
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = wordSearch
